6d8356d1af21fd312cf3e63ca7fb530fb8b2dda2
sharif9711 <sharif9711@naver.com>
2025-10-13 23:11:03 +0900

    Update project-list.js

--- js/project-list.js
+++ js/project-list.js
@@ -32,7 +32,7 @@ function renderProjects() {
                         <line x1="14" y1="11" x2="14" y2="17"></line>
                     </svg>
                 </button>
-                <div onclick="openPasswordModal('${project.id}')" class="p-6 cursor-pointer">
+                <div onclick="openProjectDirectly('${project.id}')" class="p-6 cursor-pointer">
                     <div class="flex items-start justify-between pb-3">
                         <div class="p-2 rounded-lg bg-blue-100 group-hover:bg-blue-200 transition-colors">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-blue-600">
